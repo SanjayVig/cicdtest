f042d651ea4dbfd135e5d31027679d098075a5b7
Sanjay Vig <sanjay@egregorelabs.com>
2019-01-12 14:39:38 +0530

    Repo updated

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM ubuntu:latest
 
 RUN apt-get update -y
 RUN apt-get install python -y && apt-get install python-pip
+
 RUN pip install flask
 RUN pip install waitress
-
--- userdata.sh
+++
@@ -1,9 +0,0 @@
-apt-get update -y
-apt-get install ruby -y
-apt-get install python -y
-apt-get install python-pip -y
-pip install flask pylint
-cd /home/ubuntu
-wget https://aws-codedeploy-as-south-1.s3.amazonaws.com/latest/install
-chmod +x ./install
-./install auto 
